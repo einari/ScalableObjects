3e197b41b7a6e07fd631f238539752ff4c5631e4
Einar Ingebrigtsen <einar@dolittle.com>
2016-05-21 16:29:05 +0200

    Up and running with frontend skeletons

--- Admin/PackageRoot/Code/app.js
+++ Admin/PackageRoot/Code/app.js
@@ -5,6 +5,11 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(function (request, response) {
+    response.sendFile("./public/index.html");
+});
+
+
 app.get("/test", function (request, response) {
     response.send("Hello world");
     response.sendStatus(200);
---
+++ Admin/PackageRoot/Code/public/index.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    var viewModel = function () {
+        var self = this;
+
+        this.counter = ko.observable(0);
+
+        setInterval(function () {
+            self.counter(self.counter() + 1);
+        }, 1000);
+    };
+
+    $(function () {
+        var viewModelInstance = new viewModel();
+        ko.applyBindings(viewModelInstance);
+    });
+})();
